63e7d8abed18cddd44280cb27685f08dc719874c
bfinger30 <bfinger30@gmail.com>
2025-03-10 16:41:19 -0400

    Button + validation update

--- scripts/validation.js
+++ scripts/validation.js
@@ -18,16 +18,42 @@ const checkInputValidity = (formEl, inputEl) => {
   }
 };
 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonEl) => {
+  if (hasInvalidInput(inputList)) {
+    disableButton(buttonEl);
+  } else {
+    buttonEl.disabled = false;
+    buttonEl.classList.remove("modal__button_type_error");
+  }
+};
+
+const disableButton = (buttonEl) => {
+  buttonEl.disabled = true;
+  buttonEl.classList.add("modal__button_type_error");
+};
+
+const resetValidation = (formEl, inputList) => {
+  inputList.forEach((input) => {
+    hideInputError(formEl, input);
+  });
+};
+
 const setEventListeners = (formEl) => {
   const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
-  const buttonElement = formEl.querySelector(".modal__submit-button");
+  const buttonEl = formEl.querySelector(".modal__submit-button");
 
-  // toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonEl);
 
   inputList.forEach((inputEl) => {
     inputEl.addEventListener("input", function () {
       checkInputValidity(formEl, inputEl);
-      // toggleButtonState(inputList, buttonElement);
+      toggleButtonState(inputList, buttonEl);
     });
   });
 };
